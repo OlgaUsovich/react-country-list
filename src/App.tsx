bff28190de1c658b9be911569c14515b7eefc7b3
Olga <olga@gmail.com>
2022-07-22 20:14:48 +0300

    docs(hw-37): add type for a variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { CountryList } from "./components/CountryList";
 import countries from "./country-data.json";
 import { transformCountries } from "./mappers";
+import { ICountry } from "./types";
 
 export const App = () => {
-  const transformedCountries = transformCountries(countries);
+  const transformedCountries: ICountry[] = transformCountries(countries);
   return (
     <div className="container">
       <h1 className="my-5 fs-1">Country List</h1>
